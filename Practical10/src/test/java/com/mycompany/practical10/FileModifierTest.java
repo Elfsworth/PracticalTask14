1fdce789484e243c61822435f8cfe92e8a30ac1f
Elfsworth <Elfsworth@DESKTOP-1U4IG4J>
2024-03-28 23:25:25 +0300

    Мой первый коммит

---
+++ Practical10/src/test/java/com/mycompany/practical10/FileModifierTest.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.practical10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Elfsworth
+ */
+public class FileModifierTest {
+    
+    public FileModifierTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        FileModifier.main(args);
+        fail("The test case is a prototype.");
+    }
+    
+}
